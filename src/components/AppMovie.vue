ad57793b2b27477141d772eb3fed9aa650004948
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-09 16:34:41 +0200

    add: img methods

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -3,14 +3,24 @@ export default {
     props: {
         movieObj: Object,
     },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getImageUrl(name) {
+            return new URL ('../assets/img/' + name + '.png', import.meta.url).href;
+        }
+    }
 }
 </script>
 
 <template>
     <div class="ms_movie">
-        <h2> {{ movieObj.title }} </h2>
-        <h3> {{ movieObj.original_title }}</h3>
-        <h4>{{ movieObj.original_language }}</h4>
-        <h4>{{ movieObj.vote_average }}</h4>
+        <h2 class="text-center"> {{ movieObj.title }} </h2>
+        <h3 class="text-center"> {{ movieObj.original_title }}</h3>
+        <h4 class="text-center">{{ movieObj.original_language }}</h4>
+        <h4 class="text-center">{{ movieObj.vote_average }}</h4>
     </div>
 </template>
