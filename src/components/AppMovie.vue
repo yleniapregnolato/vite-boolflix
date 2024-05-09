1d080d427da26cfb548c1b88bb63485a5cf960bc
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-09 17:51:54 +0200

    fix: flags in AppMovie

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -10,25 +10,30 @@ export default {
     },
     methods: {
         getImageUrl(name) {
-            return new URL ('../assets/img/' + name + '.png', import.meta.url).href;
+            return new URL('../assets/img/' + name + '.png', import.meta.url).href;
         }
     }
 }
 </script>
 
 <template>
-    <div class="ms_movie">
-        <h2 class="text-center"> {{ movieObj.title }} </h2>
-        <h3 class="text-center"> {{ movieObj.original_title }}</h3>
-        <div class="text-center"><img :src="getImageUrl(movieObj.original_language)" alt=""></div>
-        <h4 class="text-center">{{ movieObj.vote_average }}</h4>
+    <div class="ms_movie card">
+        <div class="card-body">
+            <h2 class="text-center"> {{ movieObj.title }} </h2>
+            <h3 class="text-center"> {{ movieObj.original_title }}</h3>
+            <div class="text-center d-flex justify-content-center">
+                <img :src="getImageUrl(movieObj.original_language)" alt="" >  
+                <h4 class="text-center ms-3">{{ movieObj.original_language }}</h4>                        
+            </div>
+            <h4 class="text-center">{{ movieObj.vote_average }}</h4>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 .ms_movie {
     img {
-        height: 30px;
+        height: 20px;
     }
 }
 </style>
