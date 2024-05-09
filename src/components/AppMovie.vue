5a847ffb0c3014624f716ca599d96825816e6f11
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-09 16:38:35 +0200

    feat: flags in AppMovie

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -20,7 +20,15 @@ export default {
     <div class="ms_movie">
         <h2 class="text-center"> {{ movieObj.title }} </h2>
         <h3 class="text-center"> {{ movieObj.original_title }}</h3>
-        <h4 class="text-center">{{ movieObj.original_language }}</h4>
+        <div class="text-center"><img :src="getImageUrl(movieObj.original_language)" alt=""></div>
         <h4 class="text-center">{{ movieObj.vote_average }}</h4>
     </div>
 </template>
+
+<style scoped lang="scss">
+.ms_movie {
+    img {
+        height: 30px;
+    }
+}
+</style>
