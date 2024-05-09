5cb7b31feb66dbe362e364fc05c238e428de2bd1
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-09 11:26:59 +0200

    feat: getResult in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <script>
 import axios from "axios";
 import { store } from "./store";
+import AppSearch from "./components/AppSearch.vue";
 export default {
-  data() {
+  components: { AppSearch },
+    data() {
     return {
       store,
     };
   },
   created() {
-   axios
-   .get("https://api.themoviedb.org/3/search/movie", {
-    params: {
-      api_key: this.store.apiKey,
-      query:"ciao",
-    },
-   })
-   .then((resp) => {
-    console.log(resp);
-   })
+    this.getResult();
+  },
+  methods: {
+    getResult() {
+      axios
+      .get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          api_key: this.store.apiKey,
+          query: this.store.searchQuery,
+        },
+      })
+      .then((resp) => {
+        this.store.moviesArray = resp.data.results;
+        console.log(resp);
+      });
+    }
   }
-}
+
+};
 </script>
+
+<template>
+  <AppSearch @filter="getResult" />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from "../store";
+export default {
+    data() {
+        return {
+            store,
+        };
+    },
+
+}
+</script>
+
+<template>
+    <div class="container-fluid d-flex justify-content-between">
+        <div class="ms_title">
+            <h1>BOOLFLIX</h1>
+        </div>
+        <div class="ms_search">
+            <input type="text" v-model="store.searchQuery">
+            <button @change="$emit('filter')">CERCA</button>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
