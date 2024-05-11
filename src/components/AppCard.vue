877142feec1c8450293efcdec3aee2f200641d03
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-11 23:55:19 +0200

    fix: voteResult in AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -19,8 +19,8 @@ export default {
 
 
         getVote() {
-            let voteRound = Math.ceil(this.cardObj.vote_average / 2);
-            return voteRound;
+            let voteResult = parseInt(this.cardObj.vote_average.toFixed() / 2);
+            return voteResult;
 
         },
     }
@@ -32,15 +32,19 @@ export default {
         <div class="card-body">
             <img v-if="cardObj.poster_path !== null" :src="getPosterUrl(cardObj.poster_path)" alt="">
             <img class="ms_not-found" v-else src="../assets/img/not-found.png" alt="not-found">
-            <h2 v-if="cardObj.title">{{ cardObj.title }}</h2>
+            <h2 v-if="cardObj.title">{{ cardObj.title }} hell</h2>
             <h2 class="text-center" v-else> {{ cardObj.name }} </h2>
             <h3 class="text-center"> {{ cardObj.original_name }}</h3>
             <div class="text-center d-flex justify-content-center">
                 <img class="language" :src="getImageUrl(cardObj.original_language)" alt="">
                 <h4 class="text-center ms-3">{{ cardObj.original_language }}</h4>
             </div>
-            <div class="text-center"><i class="fa-regular fa-star" v-for="n in 5"></i></div>
+            <div class="text-center">
+                <i class="fa-solid fa-star" v-for="n in getVote()"></i>
+                <i class="fa-regular fa-star" v-for="n in 5 - getVote()"></i>
+            </div>
         </div>
+
     </div>
 </template>
 
